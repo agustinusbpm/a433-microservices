ca2654d4f745eac3ed6602914622add54a7ce329
agustinusbpm <bagas.pm12@gmail.com>
2023-10-08 01:56:49 +0700

    Push Shell Script and cicle ci config

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Variabel yang diperlukan untuk build dan push images
+username="agustinusbpm"
+registry="ghcr.io"
+container_name="karsajobs"
+
+#Perintah untuk build image
+docker build -t $registry/$username/$container_name:latest .
+
+#Perintah untuk login ke docker registry (Github Packages)
+echo $pat | docker login $registry -u $username --password-stdin
+
+#Perintah untuk push image ke github packages
+docker push $registry/$username/$container_name:latest
