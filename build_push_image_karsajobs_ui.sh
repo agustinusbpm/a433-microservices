888e2a558ffdaef41e4c3716013a2fe474c72b00
agustinusbpm <bagas.pm12@gmail.com>
2023-10-08 18:54:07 +0700

    Initial Circle CI

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Variabel yang diperlukan untuk build dan push images
+username="agustinusbpm"
+registry="ghcr.io"
+container_name="karsajobs-ui"
+pat="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"
+
+#Perintah untuk build image
+docker build -t $registry/$username/$container_name:latest .
+
+#Perintah untuk login ke docker registry (Github Packages)
+echo $pat | docker login $registry -u $username --password-stdin
+
+#Perintah untuk push image ke github packages
+docker push $registry/$username/$container_name:latest
